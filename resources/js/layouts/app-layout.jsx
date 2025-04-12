0b3affb681dc0deca47197fc3fa6d7ec632f4160
qiffym <qiww.dev@gmail.com>
2025-04-12 15:14:23 +0800

    hero and footer

--- resources/js/layouts/app-layout.jsx
+++ resources/js/layouts/app-layout.jsx
@@ -1,5 +1,6 @@
 import { Navbar } from '@/components/navbar';
 import { Toaster } from '@/components/ui/sonner.jsx';
+import { Footer } from '@/components/footer.jsx';
 
 export function AppLayout({ children }) {
     return (
@@ -7,9 +8,10 @@ export function AppLayout({ children }) {
             <Toaster />
 
             <Navbar />
-            <main className="flex min-h-[calc(100vh_-_theme(spacing.16))] flex-1 flex-col gap-4 bg-muted/40 py-6 md:py-16">
+            <main className="flex min-h-[calc(100vh_-_theme(spacing.16))] flex-1 flex-col bg-muted/40 py-6 md:py-16">
                 {children}
             </main>
+            <Footer />
         </div>
     );
 }
